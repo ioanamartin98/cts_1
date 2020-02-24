abbfed9a5182777246269ce780bba70397bb6e94
Ioana Martin <ioana.martin98@gmail.com>
2020-02-24 13:15:38 +0200

    3rd commit

---
+++ src/lab2/Human.java
@@ -0,0 +1,33 @@
+package lab2;
+
+public class Human implements Runner {
+	
+	int age;
+	
+	public Human() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public int getSpeed() {
+		if(this.age < 50)
+			return 10;
+		else
+			return 7;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	@Override
+	public String toString() {
+		return "" + age;
+	}
+
+	
+}
